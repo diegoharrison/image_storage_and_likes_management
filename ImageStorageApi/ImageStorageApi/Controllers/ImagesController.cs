2b99a7ddc98f9cc9f8ca558f4a4e1cebdfd50146
Diego Mesquita <Diego Mesquita@DESKTOP-9EMAUMK>
2024-02-01 23:39:02 -0300

    first api

---
+++ ImageStorageApi/ImageStorageApi/Controllers/ImagesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageStorageAPI.Controllers
+{
+    [ApiController]
+    [Route("api/images")]
+    public class ImagesController : ControllerBase
+    {
+        private readonly List<string> _imageUrls = new List<string>();
+
+        public ImagesController()
+        {
+            // Inicializa com 10 imagens aleatórias do placeholder
+            for (int i = 1; i <= 10; i++)
+            {
+                _imageUrls.Add($"https://via.placeholder.com/300?text=Image{i}");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllImages()
+        {
+            return Ok(_imageUrls);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetImageById(int id)
+        {
+            if (id >= 1 && id <= _imageUrls.Count)
+            {
+                return Ok(_imageUrls[id - 1]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ ImageStorageApi/README.md
@@ -0,0 +1,17 @@
+- Instalação e Execução
+
+- Certifique-se de ter o ambiente .NET Core 8 instalado em sua máquina.
+
+- Navegue até o diretório ImageStorageApi no terminal.
+
+- Execute o seguinte comando para compilar e executar a API:
+dotnet run
+
+- A API será iniciada e estará disponível em http://localhost:7143
+com o swagger da aplicação.
+
+* Endpoints 
+
+- GET - /api/images: Retorna uma lista de imagens armazenadas.
+
+- GET - /api/images/{id}: Retorna a url pelo id da imagem
